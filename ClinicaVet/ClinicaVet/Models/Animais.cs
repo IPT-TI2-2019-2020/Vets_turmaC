330cdeb7f28e1b0ab16a3d510e0eef8d60357942
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2020-03-23 20:23:19 +0000

    Criação das classes que representam as tabelas da BD

---
+++ ClinicaVet/ClinicaVet/Models/Animais.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicaVet.Models {
+   
+   /// <summary>
+   /// Classe representa a tabela dos 'Animais' na base de dados
+   /// </summary>
+   public class Animais {
+
+      /// <summary>
+      /// PK da tabela
+      /// </summary>
+      public int ID { set; get; }
+
+      /// <summary>
+      /// Nome do animal
+      /// </summary>
+      public string  Nome { get; set; }
+
+      /// <summary>
+      /// Espécie do animal
+      /// </summary>
+      public string Especie { get; set; }
+
+      /// <summary>
+      /// Peso do animal
+      /// </summary>
+      public float Peso { get; set; }
+
+      /// <summary>
+      /// Raça do animal
+      /// </summary>
+      public string Raca { get; set; }
+
+      /// <summary>
+      /// Nome do ficheiro com a fotografia do animal
+      /// </summary>
+      public string Foto { get; set; }
+
+
+   }
+}
